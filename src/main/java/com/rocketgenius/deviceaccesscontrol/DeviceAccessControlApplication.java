10c05bbe399d139a53a0b42dcffb486979af9e2d
RockerStone <RockerStone26@outlook.com>
2021-05-06 21:28:08 +0800

    Initial commit

---
+++ src/main/java/com/rocketgenius/deviceaccesscontrol/DeviceAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.rocketgenius.deviceaccesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceAccessControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceAccessControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rocketgenius/deviceaccesscontrol/DeviceAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketgenius.deviceaccesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceAccessControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
